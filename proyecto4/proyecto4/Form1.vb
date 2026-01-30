2e67af75080dcec7bcc299382cb2dd6f8d740ab8
luis-torresugarte <luis.torresugarte@iest.edu.mx>
2026-01-30 08:24:42 -0600

    Se añadio la opcion de sumar
    leímos dos números, los convertimos de string a integer, hicimos la suma, convertimos el resultado en string y lo mostramos en pantalla

---
+++ proyecto4/proyecto4/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Numero1 As Integer = Integer.Parse(TextBox1.Text)
+        Dim Numero2 As Integer = Integer.Parse(TextBox2.Text)
+
+        Dim resultado As Integer = Numero1 + Numero2
+
+        yessir.Text = resultado.ToString()
+    End Sub
+End Class
